800e22412f821e743707e577a49e111b02c0eb15
AnnikaAlonzo <annika_alonzo@dlsu.edu.ph>
2023-04-13 23:22:23 +0800

    Modified: launch.json for datsest preparation configuration
    Added dataset preparation files for squat

--- util/visualizer.py
+++ util/visualizer.py
@@ -134,7 +134,7 @@ class Visualizer():
         webpage.add_images(ims, txts, links, width=self.win_size)
 
     def save_video(self, webpage,image_path):
-        video_name = '80seatedlongarc_annika.mp4'
+        video_name = 'video_out.mp4'
         image_dir = webpage.get_image_dir()
         save_path = os.path.join(image_dir,video_name)
         util.im2vid(image_dir, video_name)
